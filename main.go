cb2571ee3fd435e883384e64644aad006ecd29a4
Iurie Muradu <iurie.muradu@pluto.tv>
2025-06-09 16:15:04 +0200

    finalized

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	devopsconv1 "devopscon/controller/api/v1"
+	"github.com/go-logr/logr"
+	kerr "k8s.io/apimachinery/pkg/api/errors"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	"os"
@@ -13,11 +17,11 @@ import (
 type WebReconciler struct {
 	client.Client
 	scheme *runtime.Scheme
+	log    logr.Logger
 }
 
 var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
+	scheme = runtime.NewScheme()
 )
 
 // initiate the program by creating the scheme
@@ -26,6 +30,12 @@ func init() {
 }
 
 func main() {
+	// create main logger
+	logger := zap.New()
+	ctrl.SetLogger(logger)
+	log := ctrl.Log.WithName("main")
+	log.Info("set up manager")
+
 	// create manager
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme: scheme,
@@ -34,25 +44,47 @@ func main() {
 	wr := WebReconciler{
 		Client: mgr.GetClient(),
 		scheme: mgr.GetScheme(),
+		log:    log.WithName("web-reconciler"),
 	}
 
 	// create new controller
 	err = wr.SetupWithManager(mgr)
 	if err != nil {
-		setupLog.Error(err, "unable to create controller")
+		log.Error(err, "unable to create controller")
 		os.Exit(1)
 	}
 
 	// start manager
 	ctx := ctrl.SetupSignalHandler()
 	if err = mgr.Start(ctx); err != nil {
-		setupLog.Error(err, "problem running manager")
+		log.Error(err, "problem running manager")
 		os.Exit(1)
 	}
 }
 
-func (w *WebReconciler) SetupWithManager(mgr ctrl.Manager) error {
+// SetupWithManager should specify your resource explicitly
+func (wr *WebReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&devopsconv1.WebPage{}).
-		Complete(w)
+		Complete(wr)
+}
+
+func (wr *WebReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	wr.log.Info("reconciling webpage", "name", req.Name, "namespace", req.Namespace)
+
+	wp := devopsconv1.WebPage{}
+
+	err := wr.Client.Get(ctx, req.NamespacedName, &wp)
+	if err != nil && kerr.IsNotFound(err) {
+		return ctrl.Result{}, nil
+	} else if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// reconciliation logic
+	// For example, you might want to update the status of the WebPage resource, or create/update a Deployment based on the WebPage spec.
+
+	wr.log.Info("webpage reconciled")
+
+	return ctrl.Result{}, nil
 }
